88fd6ad2f76fd78bc5aa6c9dc240ba8b2ca5efe8
Noah Baldwin <nbaldwin3689@flpoly.org>
2019-01-15 15:50:41 -0500

    Update keys.js
    Fixed DB key to be universal

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,4 @@
 module.exports = {
-  mongoURI:
-    "mongodb://nbaldwin92:testing123@ds157544.mlab.com:57544/mern-boilerplate",
+  mongoURI: "mongodb://usernmae:dbpassword@linktodb",
   secretOrKey: "secret"
 };
